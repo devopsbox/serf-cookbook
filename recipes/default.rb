12e7af50e96ccaecf047eb4c461612200fe851ef
Stephan Renatus <s.renatus@cloudbau.de>
2013-12-02 12:12:23 +0100

    replace bash by ark

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,15 +1,11 @@
-version  = node[:serf][:version]
-filename = "#{version}_linux_amd64.zip"
-
 package "unzip"
 
-bash "install serf #{version}" do
-  code "cd /tmp && \
-    wget #{node[:serf][:url]} && \
-    unzip -o #{filename} && \
-    mv serf /usr/local/bin/ && \
-    rm #{filename}"
-  not_if "test -e /usr/local/bin/serf && serf version|grep #{version}"
+ark 'serf' do
+  url node[:serf][:url]
+  version node[:serf][:version]
+  path '/usr/local/bin'
+  creates 'serf'
+  action :cherry_pick
 end
 
 include_recipe 'serf::config'
